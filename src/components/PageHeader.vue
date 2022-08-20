987a4f8b945403b1d885bea8a2407e5716329686
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-20 13:26:55 -0600

    Finished the knowledge check

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <router-link to="/">Home</router-link>
+        <router-link to="/menu">Menu</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+div {
+    display: grid;
+    grid-auto-flow: column;
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <page-header></page-header>
+    </div>
+</template>
+
+<script>
+import PageHeader from '@/components/PageHeader.vue'
+    export default {
+  components: { PageHeader },
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
